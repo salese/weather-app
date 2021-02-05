da4c791109be85b794328c8a65be760801251c71
Alexander Atuh <50431402+salese@users.noreply.github.com>
2021-02-04 22:25:30 -0700

    put css file in public folder

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const https = require('https');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
