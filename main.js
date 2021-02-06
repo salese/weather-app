78dfb9940dbcdb2c8e43b86927b59d6fe756859f
Alexander Atuh <50431402+salese@users.noreply.github.com>
2021-02-06 07:34:30 -0700

    Change js file, install ejs, edit css

--- main.js
+++ main.js
@@ -4,31 +4,30 @@ const https = require('https');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.render('weatherReport', {data: ''});
 });
 
 app.post('/', (req, res) => {
-    const query = req.body.location;
+    const query = req.body.location ? req.body.location : 'calgary';
     const apiKey = 'ec15138dd4ed127fd7af31fc89d783bc';
     const unit = 'metric';
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&appid=' + apiKey + '&units=' + unit;
     
     https.get(url, (response) => {
-        response.on('data', (data) => {
-            const weatherData = JSON.parse(data);
-            const temp = Math.round(weatherData.main.temp);
-            const desc = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png>";
-
-            res.write('<p>The weather is currently: ' + desc + '</p>');
-            res.write('<h1 class=\'text-center\'>Temperature in ' + query + ' is: ' + temp + ' degrees celcius.</h1>');
-            res.write("<img src=" + imageURL);
-            res.send();
-        })
+        if(response.statusCode === 200){
+            response.on('data', (data) => {
+                const weatherData = JSON.parse(data);
+                res.render('weatherReport', {data: weatherData});
+            })
+        } else {
+            res.render('weatherReport', {data: '0'});
+        }
+        
     })
 })
 
